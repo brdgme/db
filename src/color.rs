c2e5fd78b4a2795a174c08a390759a03cedfe9d6
Michael Alexander <beefsack@gmail.com>
2017-04-06 10:37:26 +1000

    Changed color choosing to try all first prefs before moving to the next prefs

--- src/color.rs
+++ src/color.rs
@@ -40,35 +40,52 @@ impl Into<brdgme_color::Color> for Color {
 
 type LocPref = (usize, Vec<Color>);
 
+/// Choose chooses colors based on preferences. First it tries to assign all first preferences, then
+/// all second, until all have had colors assigned or it runs out of colors.
 pub fn choose(available: &HashSet<&Color>, prefs: &[Vec<Color>]) -> Vec<Color> {
     if available.is_empty() || prefs.is_empty() {
         return vec![];
     }
+    let mut sub_prefs = prefs;
+    let tail = if prefs.len() > available.len() {
+        // There are more people than available colors, so we just repeat the colours for later
+        // players.
+        let extra = choose(available, &prefs[available.len()..]);
+        sub_prefs = &prefs[..available.len()];
+        extra
+    } else {
+        vec![]
+    };
     let mut rng = rand::thread_rng();
     let mut remaining = available.clone();
     let mut assigned: HashMap<usize, Color> = HashMap::new();
-    let mut rem_prefs = filter_prefs(&remaining,
-                                     &prefs
-                                          .iter()
-                                          .enumerate()
-                                          .map(|(l, pref)| (l, pref.clone()))
-                                          .collect::<Vec<LocPref>>());
+    let mut rem_prefs = sub_prefs
+        .iter()
+        .enumerate()
+        .map(|(l, pref)| (l, pref.clone()))
+        .collect::<Vec<LocPref>>();
     rng.shuffle(&mut rem_prefs);
     'outer: loop {
         'inner: for &(pos, ref pref) in rem_prefs.clone().iter() {
             if assigned.contains_key(&pos) || pref.is_empty() {
                 continue 'inner;
             }
-            assigned.insert(pos, pref[0]);
-            remaining.remove(&pref[0]);
+            let want_color = pref[0];
+            if remaining.contains(&want_color) {
+                assigned.insert(pos, want_color);
+                remaining.remove(&want_color);
+            }
             if remaining.is_empty() {
                 // No colors left
                 break 'outer;
             }
-            rem_prefs = filter_prefs(&remaining, &rem_prefs);
         }
-        // No more preferences, exit
-        break 'outer;
+        if let Some(new_prefs) = remove_highest_prefs(&rem_prefs) {
+            rem_prefs = new_prefs;
+        } else {
+            // No more preferences, exit
+            break 'outer;
+        }
     }
     let mut left = remaining.drain();
     let mut res = vec![];
@@ -79,20 +96,28 @@ pub fn choose(available: &HashSet<&Color>, prefs: &[Vec<Color>]) -> Vec<Color> {
                      .unwrap_or_else(|| left.next().cloned().unwrap_or_else(|| Color::Green))
                      .to_owned());
     }
+    res.extend(tail);
     res
 }
 
-fn filter_prefs(available: &HashSet<&Color>, prefs: &[LocPref]) -> Vec<LocPref> {
-    prefs
+fn remove_highest_prefs(prefs: &[LocPref]) -> Option<Vec<LocPref>> {
+    let mut some_remain = false;
+    let new_prefs = prefs
         .iter()
-        .map(|&(p, ref pref)| {
-                 (p,
-                  pref.iter()
-                      .filter(|c| available.contains(c))
-                      .cloned()
-                      .collect())
-             })
-        .collect()
+        .map(|&(pos, ref pref)| {
+            let new_pref = if pref.is_empty() {
+                vec![]
+            } else {
+                let p = pref[1..].to_owned();
+                if !some_remain && !p.is_empty() {
+                    some_remain = true;
+                }
+                p
+            };
+            (pos, new_pref)
+        })
+        .collect::<Vec<LocPref>>();
+    if some_remain { Some(new_prefs) } else { None }
 }
 
 #[cfg(test)]
@@ -105,6 +130,14 @@ mod tests {
         assert_eq!(vec![Color::Amber, Color::Blue, Color::Green],
                    choose(&HashSet::from_iter(vec![Color::Amber, Color::Blue, Color::Green]
                                                   .iter()),
+                          &[vec![], vec![Color::Blue, Color::Green], vec![Color::Green]]));
+    }
+
+    #[test]
+    fn choose_with_extra_works() {
+        use std::iter::FromIterator;
+        assert_eq!(vec![Color::Amber, Color::Amber, Color::Amber],
+                   choose(&HashSet::from_iter(vec![Color::Amber].iter()),
                           &[vec![], vec![Color::Blue, Color::Green], vec![Color::Green]]));
     }
 }
